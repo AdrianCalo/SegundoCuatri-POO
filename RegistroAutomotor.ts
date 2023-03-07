8f1575638d443e38b6ec0a70cf771a3e9fc8dda7
AdrianCalo <110639622+AdrianCalo@users.noreply.github.com>
2023-03-06 23:01:33 -0300

    Add files via upload

---
+++ RegistroAutomotor.ts
@@ -0,0 +1,63 @@
+// implementamos la clase RegistroAutomotor debe soportar → 
+
+import { stringify } from "querystring";
+import { Car } from "./Car";
+import * as fs from "fs";
+
+//mostrar stock/consultar/borrar/actualziar/DarDeAlta
+export class RegistroAutomotor{
+    
+    //const array:string[]=[];
+
+    stock(array:Car[]){
+        console.log(array)
+    }
+    
+    consultar( auto:string,array:Car[]){ 
+        //console.log(auto,array)
+        let foundCar:Car[]= array.filter(elemento => (elemento['marca']=== auto) || (elemento['model']=== auto) || (elemento['engine']=== auto)||(elemento['año'] === auto));
+        if(foundCar.length){
+            console.log(`el vehiculo ${auto} ha sido encontrado`);
+            console.log(foundCar);
+            //foundCar.forEach((item:any) => console.log(item));
+            return foundCar;
+        }else{
+            console.log("no se encontro el tipo de vehiculo especificado")
+        }
+    }
+    borrar(auto:string, array:Car[]){
+        let foundCar:number= array.findIndex(auto => (array['marca']=== auto) || (array['model']=== auto) || (array['engine']=== auto) || (array['año']=== auto));
+        if(foundCar){
+            array.splice(foundCar,1)
+            console.log(auto,'fue eliminado de la lista')
+            console.log(array);
+        }else{
+            console.log(auto, 'no ha sido eliminado')
+        }
+    }
+   
+    Modifica(auto:string, array:Car[],dato:any){
+        let foundCar:number= array.findIndex(auto => (array['marca']=== auto) || (array['model']=== auto) || (array['engine']=== auto) || (array['año']=== auto));
+        if(foundCar>= 0){
+            array[foundCar]=dato;
+            console.log(`el vehiculo ${auto} a sido modificado por ${dato}`) 
+        }else{
+            console.log('no se a realizado ninguna modificacion');
+        }  
+    }
+   
+    actualizar(array:Car[]){
+        let newCar= JSON.stringify(array);// se cra una variable la cual tomara el vaor de los obejtos que estan en el array convirtiendolos a texto 
+        fs.writeFileSync("./BaseDatosAuto.json",newCar,"utf8")
+    }
+
+    DarDeAlta(auto:Car, array:Car[]){
+        array.push(auto)
+    }
+
+}
+//    saludo(){
+//        console.log("hola")
+//    }
+//}
+//export default RegistroAutomotor;
---
+++ index.ts
@@ -0,0 +1,21 @@
+import * as fs from 'fs'
+import { Car } from './Car'
+import {RegistroAutomotor}  from './RegistroAutomotor'
+
+let agencia:[]= []; 
+
+try{
+    const data= fs.readFileSync("./BaseDatosAuto.json", "utf8");
+    const jsonFile= JSON.parse(data);//.parse() convierte texto a objeto
+    agencia=jsonFile.map((item:any)=>item);
+}catch (error){
+    let mensError= "no se ha podido acceder al Archivo"
+    fs.writeFileSync("./error.txt",mensError,"utf8")
+}
+
+let camioneta=new Car('Ford','renger','2.2 turbo disel',2018);
+
+const registro= new RegistroAutomotor;
+
+//registro.consultar("falcon",agencia);
+registro.saludo();
